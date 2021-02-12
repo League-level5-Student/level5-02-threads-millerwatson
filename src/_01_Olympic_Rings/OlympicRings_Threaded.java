b24b339631b0e8feb79c6053d87ccda6dbf69a9d
millerwatson <millerwatson@users.noreply.github.com>
2021-02-11 18:33:39 -0800

    TODO: finish robot race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,70 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+import java.util.Iterator;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
 
+		Thread r = new Thread(() ->{
+			Robot red = new Robot(600, 300);
+			red.setPenColor(Color.red);
+			red.penDown();
+			for (int i = 0; i < 30; i++) {
+				red.turn(12);
+				red.move(20);
+			}
+		});
+		
+			Robot black = new Robot(400, 300);
+			black.setPenColor(Color.black);
+			black.penDown();
+			for (int i = 0; i < 30; i++) {
+				black.turn(12);
+				black.move(20);
+			}
+		});
+		
+		Thread bl = new Thread(() ->{
+			Robot blue = new Robot(200, 300);
+			blue.setPenColor(Color.blue);
+			blue.penDown();
+			for (int i = 0; i < 30; i++) {
+				blue.turn(12);
+				blue.move(20);
+			}
+		});
+		
+		Thread gr = new Thread(() ->{
+			Robot green = new Robot(500, 400);
+			green.setPenColor(Color.green);
+			green.penDown();
+			for (int i = 0; i < 30; i++) {
+				green.turn(12);
+				green.move(20);
+			}
+		});
+		
+		Thread ye = new Thread(() ->{
+			Robot yellow = new Robot(300, 400);
+			yellow.setPenColor(Color.yellow);
+			yellow.penDown();
+			for (int i = 0; i < 30; i++) {
+				yellow.turn(12);
+				yellow.move(20);
+			}
+		});
+		
+		r.start();
+		bk.start();
+		bl.start();
+		gr.start();
+		ye.start();
+	}
+
+	
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,59 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Iterator;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Random r = new Random();
+		boolean reachedTop = false;
+		Thread r1 = new Thread(() -> {
+			Robot robot1 = new Robot(50, 400);
+			robot1.setAngle(0);
+			while (robot1.getY() >= 0) {
+				robot1.move(r.nextInt(40));
+			}
+		});
+		Thread r2 = new Thread(() -> {
+			Robot robot2 = new Robot(50, 400);
+			robot2.setAngle(0);
+			while (robot2.getY() >= 0) {
+				robot2.move(r.nextInt(40));
+			}
+		});
+		Thread r3 = new Thread(() -> {
+			Robot robot3 = new Robot(50, 400);
+			robot3.setAngle(0);
+			while (robot3.getY() >= 0) {
+				robot3.move(r.nextInt(40));
+			}
+		});
+		Thread r4 = new Thread(() -> {
+			Robot robot4 = new Robot(50, 400);
+			robot4.setAngle(0);
+			while (robot4.getY() >= 0) {
+				robot4.move(r.nextInt(40));
+			}
+		});
+		Thread r5 = new Thread(() -> {
+			Robot robot5 = new Robot(50, 400);
+			robot5.setAngle(0);
+			while (robot5.getY() >= 0) {
+				robot5.move(r.nextInt(40));
+			}
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+		r5.start();
+	
+		
+	}
+	
 }
